6ede25162ec23de2027e010e1d1979a2f9c965c3
peb7268 <peb7268@gmail.com>
2017-04-28 09:35:43 -0400

    project setup

--- wp-content/themes/dd2017/index.php
+++ wp-content/themes/dd2017/index.php
@@ -1,1 +1,13 @@
-Hi
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="http://dev.duluthdiner.com/wp-content/themes/dd2017/styles/css/styles.css">
+</head>
+<body>
+    
+</body>
+</html>
