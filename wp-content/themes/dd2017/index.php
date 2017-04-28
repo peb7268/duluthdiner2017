adde518bd6cb96d5b129c9c803daa3492aae1dbd
peb7268 <peb7268@gmail.com>
2017-04-28 12:16:55 -0400

    circular logo style

--- wp-content/themes/dd2017/index.php
+++ wp-content/themes/dd2017/index.php
@@ -8,6 +8,18 @@
     <link rel="stylesheet" href="http://dev.duluthdiner.com/wp-content/themes/dd2017/styles/css/styles.css">
 </head>
 <body>
-    
+    <header>
+        <div class="wrapper">
+            <div class="logo">
+                <p class="tagline">Est. 2007</p>
+                <h1>Duluth Diner</h1>
+                <p class='slogan'>Where community meets cuisine</p>
+            </div>
+        </div>
+    </header>
+    <div class="cta">
+        <div class="wrapper">
+        </div>
+    </div>
 </body>
 </html>
