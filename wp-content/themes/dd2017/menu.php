ffe90510bdd1a112953c2831407fddc63e260f61
Paul Barrick <peb7268@gmail.com>
2019-08-30 20:59:20 -0400

    menu styling

--- wp-content/themes/dd2017/menu.php
+++ wp-content/themes/dd2017/menu.php
@@ -7,17 +7,19 @@
 <?php get_header(); ?>
     <div class="menu-wrapper">
         <div class="wrapper content menu">
+            <?php 
+                $cat = str_replace('/', '', explode('/menu/', $_SERVER['REQUEST_URI'])[1]);
+                if(strlen($cat) > 0):
+            ?> 
+
+            <div class="banner">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/menu/banners/<?php echo $cat; ?>/banner.jpg">
+            </div>
+
             <div class="menu-sidebar">
                 <?php wp_nav_menu( array( 'theme_location' => 'diner-menu' ) ); ?>
             </div>
             <div class="menu-selection">
-                <?php 
-                    $cat = str_replace('/', '', explode('/menu/', $_SERVER['REQUEST_URI'])[1]);
-                    if(strlen($cat) > 0):
-                ?> 
-                        <div class="banner">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/menu/banners/<?php echo $cat; ?>/banner.jpg">
-                        </div>
                         <div class="items">
                             <?php
                                 $args = array('post_type' => 'custom_menu', 'posts_per_page' => -1);
@@ -51,44 +53,56 @@
                         </div>
                 <?php else: ?>
                     <div class="menu-home-content">
-                        <div class="menu-banner-cta">
-                            <div class="messaging">
-                                <h2 class="tagline">
-                                    Duluth Diner Burgers
-                                </h2>
-                                <p class="slogan">
-                                    Burgers so good, momma couldnt even <br>
-                                    make them like this.
-                                </p>
+                        <div class="menu-banner">
+                            <img src="http://duluthdiner.com/wp-content/themes/dd2017/img/headers/istockphoto-1030722720-2048x2048.jpg" />
+                        </div>
+                        
+                        <div class="menu-home-body">
+                            <div class="menu-sidebar left">
+                                <?php wp_nav_menu( array( 'theme_location' => 'diner-menu' ) ); ?>
+                            </div>
+                            
+                            <div class="right">
+                                <div class="row-1">
+                                    <h4 class="menu-home-heading">order up</h4>
+                                    <ul class="home-menu-ctas">
+                                        <li>
+                                            <img src="http://duluthdiner.com/wp-content/themes/dd2017/img/ctas/grub_hub.jpg">
+                                        </li>
+                                        <li>
+                                            <a href="https://www.ubereats.com/stores/e968dff0-af39-48fe-a2b5-65adc1a95654/" target="_self">
+                                                <img src="http://duluthdiner.com/wp-content/themes/dd2017/assets/img/ubereats_banner.jpg">
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="far fa-file-alt"></i>
+                                                <span>download the menu</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                
+                                <div class="menu-home-msg">
+                                    <h4 class="menu-home-heading">Fresh Simple ingredients blended to perfection</h4>
+                                    <p>
+                                        Here at Duluth Diner, we use <span class="italics">locally sourced ingredients</span> <span class="underline">prepared fresh daily</span>.
+                                        That's the secret of how we deliver consistent premium quality at a local neighborhood price.
+                                    </p>
+                                </div>
+
+                                <div class="the-specials">
+                                    <h4 class="menu-home-heading">these are more than food. they're specials</h4>
+                                    <ul class="home-menu-specials">
+                                        <?php get_template_part('partials/loops/specials'); ?>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="menu-banner"></div>
-                        
-                        <ul class="home-menu-ctas">
-                            <li>Place a Togo order</li>
-                            <li>uber eats</li>
-                            <li>Download menu</li>
-                        </ul>
-
-                        <ul class="home-menu-specials">
-                            <li>Special 1</li>
-                            <li>Special 2</li>
-                            <li>Special 3</li>
-                            <li>Special 4</li>
-                        </ul>
                     </div>
                 <?php endif; ?>
             </div>
         </div>
     </div>
 
-    <script type="text/javascript">
-        window.addEventListener('load', onVrViewLoad)
-        function onVrViewLoad() {
-            var vrView = new VRView.Player('.menu-banner', {
-                image: 'http://dev.duluthdiner.com/wp-content/themes/dd2017/assets/img/vr.jpeg'
-            });
-        }
-    </script>
 <?php get_footer(); ?>
--- wp-content/themes/dd2017/partials/loops/specials.php
+++ wp-content/themes/dd2017/partials/loops/specials.php
@@ -4,8 +4,29 @@
 <li><a href="#"><i class="fa fa-caret-right"></i>Some awesome post title here</a></li> -->
 
 <?php $query = new Wp_Query('category_name=special'); ?>
+
+
 <?php if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-    <li><a href="<?php the_permalink(); ?>"><i class="fa fa-caret-right"></i><?php the_title(); ?></a></li>
+    <li class="standard-special">
+        <a href="<?php the_permalink(); ?>">
+            <i class="fa fa-caret-right"></i>
+            <?php the_title(); ?>
+        </a>
+    </li>
+
+    <li class="menu-post">
+        <div class="shade radial"></div>
+        <a href="<?php the_permalink(); ?>">
+            <?php
+                if ( has_post_thumbnail() ) {
+                    the_post_thumbnail('medium');
+                } 
+            ?>
+        </a>
+        <p class="special-title">
+            <?php the_title(); ?>
+        </p>
+    </li>
 <?php endwhile; wp_reset_postdata(); else : ?>
 	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
